ae4b9aa84d02b44909e33e5ca4e42da7dd57b6a0
Macke Hansson <macke.hansson@ninetech.com>
2021-06-30 14:13:18 +0200

    Added webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+    mode: "development",
     entry: "./src/index.js",
+    devtool: "inline-source-map",
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
+        clean: true,
     },
+    devServer: {
+        contentBase: "./dist",
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Output Management",
+            template: "public/index.html",
+        }),
+    ],
 };
